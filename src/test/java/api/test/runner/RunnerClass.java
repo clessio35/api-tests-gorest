e1abc441c40d8e6307ef4c18882d9c25682d75b5
Clessio <clessio.silva1989@gmail.com>
2025-04-19 11:52:31 -0300

    steps class and runner

---
+++ src/test/java/api/test/runner/RunnerClass.java
@@ -0,0 +1,5 @@
+package api.test.runner;
+
+public class RunnerClass {
+
+}
---
+++ src/test/java/api/test/steps/GoRestSteps.java
@@ -0,0 +1,80 @@
+package api.test.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoRestSteps {
+
+	@Given("que acesso a API {string}")
+	public void que_acesso_a_api(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("realizo uma request GET para {string}")
+	public void realizo_uma_request_get_para(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("eu valido a resposta da página {string} com a lista de usuários")
+	public void eu_valido_a_resposta_da_página_com_a_lista_de_usuários(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("eu valido os dados do usuário específico {string}")
+	public void eu_valido_os_dados_do_usuário_específico(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("eu valido que o erro retornado tem o status code {string}")
+	public void eu_valido_que_o_erro_retornado_tem_o_status_code(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("realizo uma request POST para {string} com {string}, {string}, {string} e {string}")
+	public void realizo_uma_request_post_para_com_e(String string, String string2, String string3, String string4, String string5) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("eu valido que a criação foi bem-sucedida com status {string}")
+	public void eu_valido_que_a_criação_foi_bem_sucedida_com_status(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("eu valido o erro retornado com status {string}")
+	public void eu_valido_o_erro_retornado_com_status(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("realizo uma request PUT para {string} com novos dados {string}, {string}")
+	public void realizo_uma_request_put_para_com_novos_dados(String string, String string2, String string3) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("eu valido que os dados foram atualizados corretamente com status {string}")
+	public void eu_valido_que_os_dados_foram_atualizados_corretamente_com_status(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("realizo uma request DELETE para {string}")
+	public void realizo_uma_request_delete_para(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("eu valido o status de resposta {string}")
+	public void eu_valido_o_status_de_resposta(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+}
